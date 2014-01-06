c407f0078ea0ec0d6c3e92f5d8523fd8b74c3aa7
jchine <jonathan.chine@gmail.com>
2014-01-06 19:59:44 +0100

    Include debug mode. Sample file
    Include debug mode in sample file

--- resources/sample.php
+++ resources/sample.php
@@ -5,6 +5,9 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+// Activate mode debug 
+$app['debug'] = true;
+
 $app->get('/hello/{name}', function ($name) use ($app) {
   return 'Hello '.$app->escape($name);
 });
